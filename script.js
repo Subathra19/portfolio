764c5cf9f559eb357f11b59e0d15a45905a1ff35
Sri Subathra Devi B <subathrad18@gmail.com>
2021-07-18 15:20:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,20 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-button').addClass("show");
+        }
+        else{
+            $('.scroll-up-button').removeClass("show");
+        }
     });
 
+    //slide-up
+    $('.scroll-up-button').click(function(){
+        $('html').animate({scrollTop: 0});
+        $('html').css("scrollBehavior", "auto");
+    });
+    
     //Toggle navigation bar
     $('.menu-button').click(function(){
         $('.navbar .menu').toggleClass("active");
